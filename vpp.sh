8eae60620c857e307004a8d9b9b03b105ad9f7ad
Junhua Zhai <junhua.zhai@outlook.com>
2024-01-25 07:58:32 +0000

    Add startup.conf

--- vpp.sh
+++ vpp.sh
@@ -5,4 +5,12 @@
 sysctl vm.nr_hugepages=512
 sysctl vm.max_map_count=1548
 
-exec vpp -c /etc/vpp/startup.conf
+/usr/bin/vpp -c ${1:-/etc/vpp/startup.conf} &
+sleep 3
+
+# Create a host interface which connects p4 bmv2 simple_switch
+HOST_INTERFACE=${HOST_INTERFACE:-veth5}
+vppctl create host-interface name $HOST_INTERFACE
+vppctl set interface state host-$HOST_INTERFACE up
+vppctl set interface promiscuous on host-$HOST_INTERFACE
+vppctl
